e8257ab4e9fb2a2be467f0ba36ca0bee6643dc01
Phaired <65019388+Phaired@users.noreply.github.com>
2025-09-12 23:21:12 +0200

    refactor: remove JSONB columns from Sessions and RuneHistories to finalize database schema

---
+++ WardenData/Migrations/20250912211638_RemoveJsonbColumnsAndFinalizeSchema.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WardenData.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveJsonbColumnsAndFinalizeSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InitialEffects",
+                table: "Sessions");
+
+            migrationBuilder.DropColumn(
+                name: "RunesPrices",
+                table: "Sessions");
+
+            migrationBuilder.DropColumn(
+                name: "EffectsAfter",
+                table: "RuneHistories");
+
+            migrationBuilder.DropColumn(
+                name: "EffectName",
+                table: "OrderEffects");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InitialEffects",
+                table: "Sessions",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RunesPrices",
+                table: "Sessions",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EffectsAfter",
+                table: "RuneHistories",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EffectName",
+                table: "OrderEffects",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
